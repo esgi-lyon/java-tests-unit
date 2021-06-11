17bb73ece87a91b07d7688678a4e159a3a4d9211
loic-roux-404 <loic.roux.404@gmail.com>
2021-06-11 12:12:20 +0200

    refacto: use centralized module clas

--- src/main/java/com/app/Cli/Main.java
+++ src/main/java/com/app/Cli/Main.java
@@ -1,16 +1,12 @@
 package com.app.Cli;
 
-import com.app.Modules.CoreModule;
-import com.app.Modules.ServiceModule;
+import com.app.InjectionModules;
 import com.framework.Exception.EntityManagerException;
-import com.framework.Services.Dao.EntityManagerModule;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class Main {
-
     public static void main(String[] args) throws EntityManagerException {
-        Injector injector = Guice.createInjector(new CoreModule(), new EntityManagerModule(), new ServiceModule());
+        Injector injector = InjectionModules.getFilledInjector();
         BuyEventProcess buyEventProcess = injector.getInstance(BuyEventProcess.class);
         buyEventProcess.process(args);
     }
